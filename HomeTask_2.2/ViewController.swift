76cb3d15f5c825d1696bffecdded084a5f248b24
polosa00 <i@apolochanin.ru>
2023-03-17 00:57:32 +0300

    Rebuild commit

--- HomeTask_2.2/ViewController.swift
+++ HomeTask_2.2/ViewController.swift
@@ -18,9 +18,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setStartingConfigurationsForViews()
         
+    }
+    
+    
+    
+    private func setStartingConfigurationsForViews() {
+        redSignalView.alpha = 0.3
+        redSignalView.layer.cornerRadius = 50
+
+        yellowSignalView.alpha = 0.3
+        yellowSignalView.layer.cornerRadius = 50
+
+        greenSignalView.alpha = 0.3
+        greenSignalView.layer.cornerRadius = 50
+
+        startButton.layer.cornerRadius = 12
     }
 
 
+    @IBAction func startedSignalAction(_ sender: Any) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        
+        
+        
+    }
 }
 
