4e7db31aad14f3c955b4b59cf7ab2a261ebd61f1
polosa00 <i@apolochanin.ru>
2023-03-16 23:29:12 +0300

    Set IBOutlets and IBAction

--- HomeTask_2.2/ViewController.swift
+++ HomeTask_2.2/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
