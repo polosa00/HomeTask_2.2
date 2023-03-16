02c4d7fc626dbc0c113e9b39ce8f358ab62e0e15
polosa00 <i@apolochanin.ru>
2023-03-16 23:17:19 +0300

    Initial Commit

---
+++ HomeTask_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask_2.2
+//
+//  Created by Александр Полочанин on 16.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
