f3ba61d20423dfe5e5d7b6aa4fdc94f7a35a4293
polosa00 <i@apolochanin.ru>
2023-03-17 09:32:56 +0300

    Create logic for color lights

--- HomeTask_2.2/ViewController.swift
+++ HomeTask_2.2/ViewController.swift
@@ -7,43 +7,59 @@
 
 import UIKit
 
+enum CurrentColor {
+    case red
+    case yellow
+    case green
+}
+
+
 class ViewController: UIViewController {
-    
     
     @IBOutlet var redSignalView: UIView!
     @IBOutlet var yellowSignalView: UIView!
     @IBOutlet var greenSignalView: UIView!
     @IBOutlet var startButton: UIButton!
     
+    private var currentColor = CurrentColor.red
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOff:CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setStartingConfigurationsForViews()
-        
     }
     
-    
-    
     private func setStartingConfigurationsForViews() {
-        redSignalView.alpha = 0.3
+        redSignalView.alpha = lightIsOff
+        yellowSignalView.alpha = lightIsOff
+        greenSignalView.alpha = lightIsOff
+        
         redSignalView.layer.cornerRadius = 50
-
-        yellowSignalView.alpha = 0.3
         yellowSignalView.layer.cornerRadius = 50
-
-        greenSignalView.alpha = 0.3
         greenSignalView.layer.cornerRadius = 50
 
         startButton.layer.cornerRadius = 12
+        startButton.setTitle("START", for: .normal)
     }
-
 
     @IBAction func startedSignalAction(_ sender: Any) {
         startButton.setTitle("NEXT", for: .normal)
         
-        
-        
-        
+        switch currentColor {
+        case .red:
+            redSignalView.alpha = lightIsOn
+            greenSignalView.alpha = lightIsOff
+            currentColor = .yellow
+        case .yellow:
+            yellowSignalView.alpha = lightIsOn
+            redSignalView.alpha = lightIsOff
+            currentColor = .green
+        case .green:
+            greenSignalView.alpha = lightIsOn
+            yellowSignalView.alpha = lightIsOff
+            currentColor = .red
+        }
     }
 }
 
